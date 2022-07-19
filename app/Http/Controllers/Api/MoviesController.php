6d1672b1d93b38534bc49fcf0d6f51480099b8b1
naufan17 <muhammadnaufan719@yahoo.com>
2022-07-19 09:10:11 +0700

    add delete data

--- app/Http/Controllers/Api/MoviesController.php
+++ app/Http/Controllers/Api/MoviesController.php
@@ -59,51 +59,18 @@ class MoviesController extends Controller
         return new MoviesResource(true, 'Data added successfully', $movies);   
     }
 
-    public function update(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'id' => 'required',
-            'title' => 'required|string',
-            'year' => 'required|string',
-            'trailer' => 'required|string',
-            'torrent' => 'required|string',
-        ]);
-
-        if($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
-        $response = Http::get('http://www.omdbapi.com', [
-            'apikey' => '7fd0d56',
-            't' => $request->title,
-            'y' => $request->year,
-        ]);
-
-        $data = json_decode($response);
-
-        $movies = Movie::where('id', $request->id)->update([
-            'title' => $data->Title,
-            'poster' => $data->Poster,
-            'year' => $data->Year,
-            'trailer' => $request->trailer,
-            'released' => $data->Released,
-            'runtime' => $data->Runtime,
-            'genre' => $data->Genre,
-            'director' => $data->Director,
-            'writer' => $data->Writer,
-            'actors' => $data->Actors,
-            'plot' => $data->Plot,
-            'torrent' => $request->torrent,
-        ]);
-
-        return new MoviesResource(true, 'Data added successfully', $movies);   
-    }
-
     public function show($id)
     {
         $movies = Movie::select('title', 'poster', 'year', 'trailer', 'released', 'runtime', 'genre', 'director', 'writer', 'actors', 'plot', 'torrent')
                         ->where('id', $id)->get();
                         
         return new MoviesResource(true, 'data was successfully obtained based on id', $movies);
+    }
+
+    public function destroy(Movie $movies)
+    {
+        $movies->delete();
+
+        return new MoviesResource(true, 'data successfully deleted', null);
     }
 }
