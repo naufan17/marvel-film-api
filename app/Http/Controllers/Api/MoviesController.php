4377a98bf2ad4276c49cf7f262112678b67929e9
naufan17 <muhammadnaufan719@yahoo.com>
2022-07-22 03:28:03 +0700

    add update and store data, revisi search data

--- app/Http/Controllers/Api/MoviesController.php
+++ app/Http/Controllers/Api/MoviesController.php
@@ -13,80 +13,104 @@ class MoviesController extends Controller
 {
     public function index()
     {
-        $movies = Movie::select('title', 'poster', 'year', 'plot')
+        $movie = Movie::select('title', 'poster', 'year', 'plot')
                         ->orderBy('year', 'desc')
                         ->get();
 
-        if($movies->isEmpty()){
+        if($movie->isEmpty()){
             return new MoviesResource(false, 'Data failed to get', null);
         }else{
-            return new MoviesResource(true, 'Data has been obtained', $movies);    
+            return new MoviesResource(true, 'Data has been obtained', $movie);    
         }
     }
 
-    // public function store(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'title' => 'required|string',
-    //         'year' => 'required|string',
-    //         'trailer' => 'required|string',
-    //         'torrent' => 'required|string',
-    //     ]);
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'year' => 'required|string',
+            'trailer' => 'required|string',
+            'torrent' => 'required|string',
+        ]);
 
-    //     if($validator->fails()) {
-    //         return response()->json($validator->errors(), 422);
-    //     }
+        // if($validator->fails()) {
+        //     return response()->json($validator->errors(), 422);
+        // }
 
-    //     $response = Http::get('http://www.omdbapi.com', [
-    //         'apikey' => '7fd0d56',
-    //         't' => $request->title,
-    //         'y' => $request->year,
-    //     ]);
+        $response = Http::get('http://www.omdbapi.com', [
+            'apikey' => '7fd0d56',
+            't' => $request->title,
+            'y' => $request->year,
+        ]);
 
-    //     $data = json_decode($response);
+        $data = json_decode($response);
 
-    //     $movies = Movie::create([
-    //         'title' => $data->Title,
-    //         'poster' => $data->Poster,
-    //         'year' => $request->year,
-    //         'trailer' => $request->trailer,
-    //         'released' => $data->Released,
-    //         'runtime' => $data->Runtime,
-    //         'genre' => $data->Genre,
-    //         'director' => $data->Director,
-    //         'writer' => $data->Writer,
-    //         'actors' => $data->Actors,
-    //         'plot' => $data->Plot,
-    //         'torrent' => $request->torrent,
-    //     ]);
+        $movie = Movie::create([
+            'title' => $data->Title,
+            'poster' => $data->Poster,
+            'year' => $request->year,
+            'trailer' => $request->trailer,
+            'released' => $data->Released,
+            'runtime' => $data->Runtime,
+            'genre' => $data->Genre,
+            'director' => $data->Director,
+            'writer' => $data->Writer,
+            'actors' => $data->Actors,
+            'plot' => $data->Plot,
+            'torrent' => $request->torrent,
+        ]);
 
-    //     if($movies->isEmpty()){
-    //         return new MoviesResource(false, 'Data failed to store', null);
-    //     }else{
-    //         return new MoviesResource(true, 'Data stored successfully', $movies);    
-    //     }
-    // }
+        if($movie->isEmpty()){
+            return new MoviesResource(false, 'Data failed to store', null);
+        }else{
+            return new MoviesResource(true, 'Data stored successfully', $movie);    
+        }
+    }
+
+    public function update(Request $request, Movie $movie)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'year' => 'required|string',
+            'trailer' => 'required|string',
+            'torrent' => 'required|string',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $movie->update([
+            'title' => $request->title,
+            'year' => $request->year,
+            'trailer' => $request->trailer,
+            'torrent' => $request->torrent,
+        ]);
+
+        return new MoviesResource(true, 'Data updated successfully', $movie);    
+    }
 
     public function show($title)
     {
-        $movies = Movie::select('title', 'poster', 'year', 'trailer', 'released', 'runtime', 'genre', 'director', 'writer', 'actors', 'plot', 'torrent')
+        $title = ucfirst($title);
+        $movie = Movie::select('title', 'poster', 'year', 'trailer', 'released', 'runtime', 'genre', 'director', 'writer', 'actors', 'plot', 'torrent')
                         ->where('title', 'LIKE', '%'.$title.'%')->get();
                         
-        if($movies->isEmpty()){
+        if($movie->isEmpty()){
             return new MoviesResource(false, 'Data failed to get', null);
         }else{
-            return new MoviesResource(true, 'Data has been obtained', $movies);    
+            return new MoviesResource(true, 'Data has been obtained', $movie);    
         }
     }
 
     // public function destroy($id)
     // {
-    //     $movies = Movie::where('id', $id)->delete();
+    //     $movie = Movie::where('id', $id)->delete();
 
-    //     if($movies->isEmpty()){
+    //     if($movie->isEmpty()){
     //         return new MoviesResource(false, 'Data failed to delete', null);
     //     }else{
-    //         return new MoviesResource(true, 'Data deleted successfully', $movies);    
+    //         return new MoviesResource(true, 'Data deleted successfully', $movie);    
     //     }
     // }
 }
