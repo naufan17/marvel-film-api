f2eed9851521f595194784490e62f5f741661a8f
naufan17 <muhammadnaufan719@yahoo.com>
2022-08-02 15:48:38 +0700

    fix error tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,11 @@
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <style>
+            body{
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
     </head>
     <body class="antialiased ">
         <div class="relative bg-gray-800">
